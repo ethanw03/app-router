dc76c9f5f799fa09093fa4ca98bf8cbfbcc72ed7
ethanw03 <ethanwong98@yahoo.ca>
2024-02-25 02:42:00 -0800

    Add toggle for complete to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,14 @@ function getTodos() {
   return prisma.todo.findMany();
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  'use server';
+
+  console.log(id, complete);
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
+}
+
 export default async function Home() {
   const todos = await getTodos();
 
@@ -25,6 +33,7 @@ export default async function Home() {
           <TodoItem
             key={todo.id}
             {...todo}
+            toggleTodo={toggleTodo}
           />
         ))}
       </ul>
